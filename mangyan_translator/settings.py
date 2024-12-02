d5a12172f4c554f254c21e65d85a7563ab81ab7a
Ken <kennyy762@gmail.com>
2024-12-02 11:02:03 +0800

    with camera/ backup

--- mangyan_translator/settings.py
+++ mangyan_translator/settings.py
@@ -119,6 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Use your base directory here
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
